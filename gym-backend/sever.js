2b165dc11012dae15559f8de45d0c5aaab2d96a8
JeromeAmiteye1 <amiteye25jerome@gmail.com>
2025-08-19 01:27:06 +0000

    f you

--- gym-backend/sever.js
+++ gym-backend/sever.js
@@ -1,1 +1,22 @@
-console.log("jj is good");
+const express = require('express');
+     const cors = require('cors');
+
+     const app = express();
+     const port = process.env.PORT || 3000;
+
+     app.use(cors());
+     app.use(express.json());
+
+     app.get('/', (req, res) => {
+       console.log('Ji is good');
+       res.json({ message: 'Fitness App Backend - Free Version' });
+     });
+db.students.insertOne({
+  graduationDate: null,
+  courses: ["Biology", "Chemistry", "Calculus"],
+  address: {
+    street: "123 Fake st.",
+    city: "Bikini Bottom",
+    zip: 12345
+  }
+})
