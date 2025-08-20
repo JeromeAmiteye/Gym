6743c372bf3f6881abe088fa2b2d00a2d6d4dd08
Richard <rj8411460@gmail.com>
2025-08-20 13:35:51 +0000

    ...

--- gym-backend/sever.js
+++ gym-backend/sever.js
@@ -1,22 +1,22 @@
-const express = require('express');
-     const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-     const app = express();
-     const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-     app.use(cors());
-     app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-     app.get('/', (req, res) => {
-       console.log('Ji is good');
-       res.json({ message: 'Fitness App Backend - Free Version' });
-     });
+app.get("/", (req, res) => {
+  console.log("Ji is good");
+  res.json({ message: "Fitness App Backend - Free Version" });
+});
 db.students.insertOne({
   graduationDate: null,
   courses: ["Biology", "Chemistry", "Calculus"],
   address: {
     street: "123 Fake st.",
     city: "Bikini Bottom",
-    zip: 12345
-  }
-})
+    zip: 12345,
+  },
+});
